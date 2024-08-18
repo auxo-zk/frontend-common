fed69d659db6ef60c3e4f31014767c5cc1125e09
Le Dinh Quyen <quyenld9699@gmail.com>
2024-08-18 09:32:58 +0700

    update data

--- lib/constants/contractAddress.ts
+++ lib/constants/contractAddress.ts
@@ -4,8 +4,8 @@ import { Address } from 'viem';
 export type ContractName = 'GovernorFactory' | 'Campaign' | 'TokenFund';
 export const contractAddress: Record<AppChainId, Record<ContractName, Address>> = {
     [openCampusCodex.id]: {
-        GovernorFactory: '0x5C1731263FB90AB046282aaa789109DdF15755ba',
-        Campaign: '0x8addD204ef28ebC1467f67AE9AB73a3d4b718459',
+        GovernorFactory: '0xe04F111eE5616B7741b5d9aA8836528c29B837AE',
+        Campaign: '0x6dF9314e6849aC82bFe7b71898A0AA46134175bE',
         TokenFund: '0xd35f12eF94db3d84Bbf2a43F2537cF294C7E6717',
     },
     [auxoDevNet.id]: {
