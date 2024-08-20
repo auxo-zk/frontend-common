15c57f67b323047f6c6872bb6ab92a160eeced34
Le Dinh Quyen <quyenld9699@gmail.com>
2024-08-20 22:48:00 +0700

    update toast

--- lib/components/ToastNotifier/ToastNotifier.tsx
+++ lib/components/ToastNotifier/ToastNotifier.tsx
@@ -2,5 +2,5 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function ToastNotifier() {
-    return <ToastContainer autoClose={3000} newestOnTop={true} />;
+    return <ToastContainer autoClose={3000} newestOnTop={true} style={{ width: 'auto', maxWidth: '90%', minWidth: '280px' }} />;
 }
