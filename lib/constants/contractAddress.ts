a94c854736066f8b0a22c02a9d9e516e089beb1c
huyminh1115 <huyminh1115@gmail.com>
2024-08-28 20:51:09 +0700

    Change contract address

--- lib/constants/contractAddress.ts
+++ lib/constants/contractAddress.ts
@@ -4,8 +4,8 @@ import { Address } from 'viem';
 export type ContractName = 'GovernorFactory' | 'Campaign' | 'TokenFund';
 export const contractAddress: Record<AppChainId, Record<ContractName, Address>> = {
     [openCampusCodex.id]: {
-        GovernorFactory: '0x7F5BBCc804A94c4d450871612993cBE480Fe0E22',
-        Campaign: '0x07c2A3893b5c30A32015D8D7f869749b97CEc821',
+        GovernorFactory: '0x46807C2576FA86C5740e7f580940596D0fD4B3EF',
+        Campaign: '0x2890C43A533CE1fEEcB92BD6751831183C506E3f',
         TokenFund: '0xd35f12eF94db3d84Bbf2a43F2537cF294C7E6717',
     },
     [auxoDevNet.id]: {
